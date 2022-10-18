8cbe58b42519ae382a689b7556096b7c89ebe0a5
sickopam <sickopam@gmail.com>
2022-10-18 23:22:28 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1 className='text-slate-800 text-xl'>Hello WORLD</h1>
-      <h2 className='text-3xl text-red-300'>push cok gabisa paste img</h2>
-      sini ae femm      
+      <Home/>   
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import homepic from "../img/homepagebg.png"
 
 export default function Home() {
   return (
-    <div className='h-screen'>
-        
+    <div className='h-auto'>
+      <div className='h-full w-full'>
+        <img className='object-fill' src={homepic} alt="renaissance"></img>
+      </div>
+
+      <div className='text-black z-10 absolute top-0 left-0'>
+        <h1>hello</h1>
+      </div>
     </div>
   )
 }
