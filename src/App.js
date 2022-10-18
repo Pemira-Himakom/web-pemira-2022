c8e2d76b12ecc17670c759f9ec41273a47a725c0
sickopam <sickopam@gmail.com>
2022-10-18 23:09:20 +0700

    test

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1 className='text-slate-800 text-xl'>Hello WORLD</h1>
-      <h2 className='text-3xl text-red-300'>Opam Ngentot</h2>
+      <h2 className='text-3xl text-red-300'>Opam dsncsod</h2>
     </div>
   );
 }
