9f6576f2e35103589c7ea426decf81ebf81c265b
Rifqifam <muhammad.rifqi.fameizy@mail.ugm.ac.id>
2022-10-18 23:07:29 +0700

    added image

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ function App() {
     <div className="App">
       <h1 className='text-slate-800 text-xl'>Hello WORLD</h1>
       <h2 className='text-3xl text-red-300'>push cok gabisa paste img</h2>
-      kok gaad img ama components folder nya ya
-      
+      sini ae femm      
     </div>
   );
 }
