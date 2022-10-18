47144f12a7e620f0e0a76691d756cd263f762b6a
Rifqifam <muhammad.rifqi.fameizy@mail.ugm.ac.id>
2022-10-18 22:46:29 +0700

    rev

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ function App() {
   return (
     <div className="App">
       <h1 className='text-slate-800 text-xl'>Hello WORLD</h1>
-      <h2 className='text-3xl text-red-300'>Opam Ngentot</h2>
+      <h2 className='text-3xl text-red-300'>push cok gabisa paste img</h2>
+      
     </div>
   );
 }
