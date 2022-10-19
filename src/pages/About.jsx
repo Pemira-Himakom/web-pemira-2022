1c79e0a3f50d2886fb6f206c10e22363d2706e3e
sickopam <sickopam@gmail.com>
2022-10-19 17:41:35 +0700

    home design

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
+import Hima from '../img/himakom.PNG'
 
 const About = () => {
   return (
-    <div class="bg-white flex content-evenly p-5">
-        <div className='bg-prim-color w-[50%] h-[70vh] flex items-center justify-center'>
-          <img src='' alt="" />
+    <div class="bg-white flex content-evenly">
+        <div className='bg-prim-color w-[50%] h-[100vh] flex items-center justify-center'>
+          <img className='scale-75' src={Hima} alt="himakom" />
         </div>
-        <div className='bg-white w-[50%] max-w-[35rem] m-auto text-justify text-xl px-5'>
+        <div className='bg-white w-[50%] max-w-[35rem] m-auto text-justify text-xl p-5'>
 
-          <h1 className='text-center mb-10 text-7xl prim-color font-garamond'>About Pemira</h1>
+          <h1 className='text-center mb-10 text-6xl prim-color'>About Pemira</h1>
 
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum tellus.
+          <p className='font-light'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum tellus.
           </p>
         </div>
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,8 @@ export default function Home() {
     <div>
       {/* title */}
       <header className="flex items-center justify-center h-screen bg-fixed bg-center bg-cover bg-img sm:pb-36">
-        <div className="text-center text-black font-garamond title-sm title-lg prim-color">
-          <h1 className=''>Pemilihan Raya Himakom 2022</h1>
+        <div className="text-center title-sm title-lg prim-color">
+          <h1 className='tracking-wide'>Pemilihan Raya Himakom 2022</h1>
         </div>
       </header>
     </div>
