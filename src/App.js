f38868859a6109fd25b4b9cd396e3e9c09cd73ed
Rifqifam <muhammad.rifqi.fameizy@mail.ugm.ac.id>
2022-10-20 21:54:42 +0700

    added timeline page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Home from './pages/Home';
 import About from './pages/About';
-import Faq from './pages/FAQ';
+// import Faq from './pages/FAQ';
 import Timeline from './pages/Timeline';
 
 function App() {
--- src/pages/Timeline.jsx
+++ src/pages/Timeline.jsx
@@ -1,26 +1,84 @@
 import React from 'react'
 import Star1 from '../img/star1.png'
 import Star2 from '../img/star2.png'
-import Flower from '../img/flower.png'
-import Flower2 from '../img/flower2.png'
-
+import {GoTriangleRight} from 'react-icons/go'
+import {GoTriangleLeft} from 'react-icons/go'
+import Star3 from '../img/star3.png'
 
 export default function Timeline() {
   return (
-    <div className='bg-[#21201D] h-[60vh] pt-12 prim-color font-sulangor'>
-        <div className='flex justify-between'>
-            <div className='w-[13rem] flex items-start'>
-                <img className='' src={Flower} alt='flower'/>
-            </div>
-            <div className='h-fit flex justify-evenly gap-x-12'>
+    <div className='bg-[#21201D] h-[60vh] w-screen pt-12 prim-color font-sulangor timeline-bg'>
+        <div className='flex justify-center'>
+            <div className='h-fit flex  gap-x-12'>
                 <img src={Star1} alt='star'/>
                 <h1 className='text-2xl lg:text-5xl grid items-center'>Timeline</h1>
                 <img src={Star2} alt='star'/>
             </div>
-            <div className='w-[13rem] flex items-start'>
-                <img className='' src={Flower2} alt='flower'/>
-            </div>
+        </div>
 
+        {/* Start Timeline Item */}
+        <div className='process-wrapper '>
+            <div className='progress-bar-container'>
+                
+                <ul>
+                    <li className='step'>
+                        <div className='step-inner'>
+                            <h1>17-28 Okt</h1>
+                            <h1>Open Registration</h1>
+                            <div className='circle'><img src={Star3} alt="" /></div>
+                        </div>
+                    </li>
+
+                    <li className='step'>
+                        <div className='step-inner'>
+                            <h1>17-28 Okt</h1>
+                            <h1>Open Registration</h1>
+                            <div className='circle'><img src={Star3} alt="" /></div>
+                        </div>
+                    </li>
+
+                    <li className='step'>
+                        <div className='step-inner'>
+                            <h1>17-28 Okt</h1>
+                            <h1>Open Registration</h1>
+                            <div className='circle'><img src={Star3} alt="" /></div>
+                        </div>
+                    </li>
+
+                    <li className='step'>
+                        <div className='step-inner'>
+                            <h1>17-28 Okt</h1>
+                            <h1>Open Registration</h1>
+                            <div className='circle'><img src={Star3} alt="" /></div>
+                        </div>
+                    </li>
+
+                    <li className='step a
+                    ctive'><div className='step-inner'>
+                            <h1>17-28 Okt</h1>
+                            <h1>Open Registration</h1>
+                            <div className='circle'><img src={Star3} alt="" /></div>
+                  
+                     
+                      </div></li>
+                    <li className='step'>
+                        <div className='step-inner'>
+                            <h1>17-28 Okt</h1>
+                            <h1>Open Registration</h1>
+                            <div className='circle'><img src={Star3} alt="" /></div>
+                        </div>
+                    </li>
+
+                    
+                </ul>
+
+                <div id='line'>
+                    <GoTriangleRight size={35} className='absolute right-[98%] bottom-[-15px]'/>
+                    <GoTriangleLeft size={35} className='absolute left-[98%] bottom-[-15px]'/>
+                    
+                </div>
+
+            </div>
         </div>
     </div>
   )
