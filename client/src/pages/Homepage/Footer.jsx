41513668c8e37002cc231a2a396605b17f0fc0f7
Rifqifam <muhammad.rifqi.fameizy@mail.ugm.ac.id>
2022-10-31 22:39:16 +0700

    finished footer

--- client/src/pages/Homepage/Footer.jsx
+++ client/src/pages/Homepage/Footer.jsx
@@ -1,28 +1,53 @@
 import React from 'react'
 import crown from '../../img/crown.png'
+import star5 from '../../img/Star5.png'
 
 const Footer = () => {
   return (
-    <div className='h-fit w-screen bg-[#21201D] flex flex-col'>
+    <div className='h-fit w-screen bg-[#21201D] flex flex-col font-prata'>
         {/* Pre Footer */}
-        <div className='h-screen w-screen flex flex-row'>
-            <div className='w-[22.5%] bg-red-100'>
+        <div className='h-screen w-screen flex flex-row border-b-[4px] border-[#D4A976]'>
 
+            <div className='w-[10%] lg:w-[22.5%]  flex flex-col'>
+              <div className='content  h-[60%]'></div>
+              <div className=' h-[40%] flex justify-center'>
+                <img src={star5} alt=""  className='w-[30%] m-auto'/>
+              </div>
             </div>
 
-            <div className='w-[55%] bg-red-300'>
-                <div className='flex border rounded-t-full'>
-                    <img className="m-auto object-cover" src={crown} alt="" />
+            <div className='w-[80%] lg:w-[55%]  relative flex flex-col justify-center'>
+
+              <div className='content w-full h-[10%] '></div>
+
+              <div className='h-[90%] w-full overflow-hidden pt-8 px-7 rounded-t-[500px] border-[4px] border-b-0 border-[#D4A976] relative'>
+
+                <div className='absolute top-[70%] left-[50%] -translate-x-1/2 -translate-y-1/2 w-[100%] flex flex-col justify-center'>
+                  <h1 className='text-[35px] lg:text-[90px] text-white text-center leading-none pb-5'>Pledge Your Allegiance</h1>
+
+                  <div className='w-[72%] m-auto text-[#D4A976]'>
+                    <h2 className='text-[15px] lg:text-[38px] text-center'>Tentukan Pilihanmu pada</h2>
+                    <h2 className='text-[15px] lg:text-[38px] text-center'>9-16 November</h2>
+                  </div>
+
                 </div>
+
+                <img src={crown} alt="" className='object-cover h-full lg:h-auto w-full rounded-t-[500px] border-[4px] border-b-0 border-[#D4A976] crown' />
+
+              </div>
+
             </div>
 
-            <div className='w-[22.5%] bg-red-200'>
-
+            <div className='w-[10%] lg:w-[22.5%]   flex flex-col'>
+              <div className='content h-[60%]'></div>
+              <div className=' h-[40%] flex justify-center'>
+                <img src={star5} alt=""  className='w-[40%] m-auto'/>
+              </div>
             </div>
+
         </div>
 
         {/* Footer */}
-        <div className=''>
+        <div className='h-[30%]'>
 
         </div>
 
