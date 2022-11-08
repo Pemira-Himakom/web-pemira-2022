75c69aff4244903052f61c07c7e63d89582c2a21
hadzami10 <achmad.had2003@mail.ugm.ac.id>
2022-11-08 10:36:28 +0700

    edit package json & create api function asssign

--- src/api/assign.js
+++ src/api/assign.js
@@ -1,6 +1,8 @@
+const BASE_URL = process.env.REACT_APP_SERVER_BASE_URL
+
 export const assign = async () => {
     const accessToken = localStorage.getItem('accessToken')
-    const response = await fetch('/api/admin/assign', {
+    const response = await fetch(`BASE_URL/admin/assign`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
